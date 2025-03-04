7dd4fd2f560ca910448be8b9110429dc80ea05f4
LilSPinachBoy1 <sam@moocows.co.uk>
2025-03-04 21:15:08 +0000

    Added wonder html pages and finished redirect buttons

--- home_script.js
+++ home_script.js
@@ -1,35 +1,18 @@
 function redirect(loc_index) {
 
-    // Change to an array and access with index?
+    // Array of the locations to redirect to
+    let loc_array = [
+        "homepage.html",
+        "GPoG/giza.html",
+        "GWoC/wall.html",
+        "Petra/petra.html",
+        "Colosseum/colosseum.html",
+        "Chichen/chichen.html",
+        "Machu/machu.html",
+        "Taj/taj.html",
+        "Redeemer/CtR.html"
+    ]
 
-    switch (loc_index) {
-        case 0:  // Home page
-            window.location = "homepage.html";
-            break;
-        case 1:  // Giza
-            window.location = "GPoG/giza.html";
-            break;
-        case 2:  // China
-            window.location = "GWoC/wall.html";
-            break;
-        case 3:  // Petra
-            window.location = "Petra/petra.html";
-            break;
-        case 4:  // Colosseum
-            window.location = "Colosseum/colosseum.html";
-            break;
-        case 5:  // Chichen
-            window.location = "Chichen/chichen.html";
-            break;
-        case 6:  // Machu
-            window.location = "Machu/machu.html";
-            break;
-        case 7:  // Taj
-            window.location = "Taj/taj.html";
-            break;
-        case 8:  // Redeemer
-            window.location = ""
-        default:
-            console.log("Error: could not match index to a page")
-    }
+    // Change the window location based on the index provided
+    window.location = loc_array[loc_index];
 }
